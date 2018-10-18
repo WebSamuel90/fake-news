62ad22c00ae316a5fcb06a9b1078c0d5f9089c23
Samuel <sumlebulle@hotmail.com>
2018-10-18 11:33:07 +0200

    Added  to data.php and a randomDate() to functions.php

--- data.php
+++ data.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require __DIR__.'/functions.php';
+
 
 $heroes = [
 
@@ -48,5 +50,109 @@ $heroes = [
     'id' => 5,
     'link' => 'https://en.wikipedia.org/wiki/Hulk_(comics)',
     'img' => '/assets/hulk.jpeg',
+  ]
+];
+
+
+$comments = [
+
+  [
+    'title' => 'It\'s not who I am underneath but what I do that defines me.',
+    'author' => $heroes['batman']['name'],
+    'link' => $heroes['batman']['link'],
+    'avatar' => $heroes['batman']['img'],
+    'content' => "Hero can be anyone. Even a man knowing something as simple and reassuring as putting a coat around a young boy shoulders to let him know the world hadn't ended.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'I\'m Batman',
+    'author' => $heroes['batman']['name'],
+    'link' => $heroes['batman']['link'],
+    'avatar' => $heroes['batman']['img'],
+    'content' => "My anger outweights my guilt.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'You’re much stronger than you think you are, trust me.',
+    'author' => $heroes['superman']['name'],
+    'link' => $heroes['superman']['link'],
+    'avatar' => $heroes['superman']['img'],
+    'content' => "There is a superhero in all of us, we just need the courage to put on the cape.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'It’s not an S, on my world it means hope.',
+    'author' => $heroes['superman']['name'],
+    'link' => $heroes['superman']['link'],
+    'avatar' => $heroes['superman']['img'],
+    'content' => "You will be different, sometimes you’ll feel like an outcast, but you’ll never be alone.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'Heroes are made by the paths they choose, not the powers they are graced with.',
+    'author' => $heroes['ironman']['name'],
+    'link' => $heroes['ironman']['link'],
+    'avatar' => $heroes['ironman']['img'],
+    'content' => "You can take away my suits, you can take away my home, but there’s one thing you can never take away from me: I AM IRON MAN.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'I shouldn’t be alive, unless it was for a reason, I know what I have to do, and I know it is right.',
+    'author' => $heroes['ironman']['name'],
+    'link' => $heroes['ironman']['link'],
+    'avatar' => $heroes['ironman']['img'],
+    'content' => "I’m Tony Stark. I build neat stuff, got a great girl, occasionally save the world. So why can’t I sleep?",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'I\'m... Captain America.',
+    'author' => $heroes['captain']['name'],
+    'link' => $heroes['captain']['link'],
+    'avatar' => $heroes['captain']['img'],
+    'content' => "Pretty much.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'I got beat up in that alley. And that parking lot. And behind that diner.',
+    'author' => $heroes['captain']['name'],
+    'link' => $heroes['captain']['link'],
+    'avatar' => $heroes['captain']['img'],
+    'content' => "I don't want to kill anyone, but I don't like bullies; I don't care where they're from.",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'Hulk hate puny banner!',
+    'author' => $heroes['hulk']['name'],
+    'link' => $heroes['hulk']['link'],
+    'avatar' => $heroes['hulk']['img'],
+    'content' => "Stupid big lady! Do what Hulk wants! Or Hulk will smash you, and take your kitties!",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
+  ],
+
+  [
+    'title' => 'PUNY HUMANS',
+    'author' => $heroes['hulk']['name'],
+    'link' => $heroes['hulk']['link'],
+    'avatar' => $heroes['hulk']['img'],
+    'content' => "HULK IS NOT AFRAID...HULK IS STRONGEST ONE THERE IS!!!",
+    'date' => $randomDate('2018-01-01', '2018.12.31'),
+    'likes' => rand(10, 100)
   ]
 ];
--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+
+function randomDate($sStartDate, $sEndDate, $sFormat = 'Y-m-d')
+{
+    // Convert the supplied date to timestamp
+    $fMin = strtotime($sStartDate);
+    $fMax = strtotime($sEndDate);
+    // Generate a random number from the start and end dates
+    $fVal = mt_rand($fMin, $fMax);
+    // Convert back to the specified date format
+    return date($sFormat, $fVal);
+}

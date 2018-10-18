e1b9ecf85442619c8d7b2ed54c6334709d611835
Samuel <sumlebulle@hotmail.com>
2018-10-18 09:40:13 +0200

    Added an array with authors to data.php and created an assetsfolder with images

--- data.php
+++ data.php
@@ -1,3 +1,52 @@
 <?php
 
 declare(strict_types=1);
+
+
+$heroes = [
+
+  'batman' =>
+  [
+    'name' => 'Batman',
+    'fullName' => 'Bruce Wayne',
+    'id' => 1,
+    'link' => 'https://en.wikipedia.org/wiki/Batman',
+    'img' => '/assets/batman.jpeg',
+  ],
+
+  'superman' =>
+  [
+    'name' => 'Superman',
+    'fullName' => 'Clark Kent',
+    'id' => 2,
+    'link' => 'https://en.wikipedia.org/wiki/Superman',
+    'img' => '/assets/superman.jpeg',
+  ],
+
+  'ironman' =>
+  [
+    'name' => 'Iron Man',
+    'fullName' => 'Tony Stark',
+    'id' => 3,
+    'link' => 'https://en.wikipedia.org/wiki/Iron_Man',
+    'img' => '/assets/ironman.jpeg',
+  ],
+
+  'captain' =>
+  [
+    'name' => 'Captain America',
+    'fullName' => 'Steve Rogers',
+    'id' => 4,
+    'link' => 'https://en.wikipedia.org/wiki/Captain_America',
+    'img' => '/assets/captain.jpeg',
+  ],
+
+  'hulk' =>
+  [
+    'name' => 'Hulk',
+    'fullName' => 'Bruce Banner',
+    'id' => 5,
+    'link' => 'https://en.wikipedia.org/wiki/Hulk_(comics)',
+    'img' => '/assets/hulk.jpeg',
+  ]
+];

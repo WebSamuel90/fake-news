7a863dc82b62c6be9f2c3751fa27eb876d61d020
Samuel <sumlebulle@hotmail.com>
2018-10-31 11:50:41 +0100

    Added filter_sanitize_string to if sort statement

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ require __DIR__.'/header.php'; //concatination ;)
 require __DIR__.'/functions.php';
 require __DIR__.'/data.php';
 
-  if (isset($_GET['sortBy']) && $_GET['sortBy'] === 'likes') {
-    usort($articles, 'sortByLikes');
-  } else {
-    usort($articles, 'sortByDate');
+  if (isset($_GET['sortBy'])) {
+    $sort = filter_var($_GET['sortBy'], FILTER_SANITIZE_STRING);
+    if (($_GET['sortBy'] === 'likes')){
+        usort($articles, 'sortByLikes');
+      } else {
+        usort($articles, 'sortByDate');
+      }
   }
   require __DIR__.'/posts.php';
 
